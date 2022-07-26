d2ffb1277603e0530d0de877b95f0f4cb71bdb2f
aquatic <aquaticone@protonmail.com>
2022-07-26 17:01:36 -0600

    feat: write result to file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv-flow"
 dotenv.config()
 
+import fs from "fs/promises"
 import { Alchemy, Network } from "alchemy-sdk"
 import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
+import path from "path"
 
 yargs(hideBin(process.argv))
   .command("$0", "Take snapshot", () => {}, takeSnapshot)
@@ -14,31 +16,72 @@ yargs(hideBin(process.argv))
     description:
       "NFT contract address. Use multiple times to find intersection of holders for multiple NFTs.",
   })
+  .option("save", {
+    alias: "s",
+    type: "boolean",
+    description: "Write snapshot result to file in ./snapshots",
+  })
+  .option("name", {
+    alias: "n",
+    type: "string",
+    description: "Supplemental prefix for filename for easier identification",
+  })
   .demandOption(["c"])
   .parse()
 
-async function takeSnapshot({ contract: contracts }) {
+async function takeSnapshot({
+  contract: contracts,
+  name: filenamePrefix,
+  save,
+}) {
   const alchemy = new Alchemy({
     apiKey: process.env.ALCHEMY_KEY,
     network: Network.ETH_MAINNET,
   })
   const holders = []
+
   for (let i = 0; i < contracts.length; i++) {
     holders.push((await alchemy.nft.getOwnersForContract(contracts[i])).owners)
   }
-  if (holders.length === 0) {
+
+  if (!holders.length) {
     console.log("No holders found")
     return
   }
-  if (holders.length === 1) {
-    console.log(holders[0].join("\n"))
-    return
+
+  const commonHolders =
+    holders.length === 1
+      ? holders[0]
+      : holders.reduce((commonHolders, addresses) =>
+          commonHolders.filter((address) => addresses.includes(address))
+        )
+  const output = commonHolders.join("\n")
+
+  if (save) {
+    const filename = getFilename(contracts, filenamePrefix)
+    await saveResult(filename, output)
   }
+
   console.log(
-    holders
-      .reduce((commonHolders, addresses) =>
-        commonHolders.filter((address) => addresses.includes(address))
-      )
-      .join("\n")
+    `${output}\n==========================================\n${commonHolders.length} holders in common`
+  )
+}
+
+function getFilename(contracts, filenamePrefix) {
+  return contracts
+    .reduce(
+      (parts, contract) => [...parts, contract],
+      filenamePrefix
+        ? [filenamePrefix, new Date().toISOString()]
+        : [new Date().toISOString()]
+    )
+    .join("_")
+}
+
+async function saveResult(filename, content) {
+  await fs.writeFile(
+    path.join(process.cwd(), "snapshots", filename),
+    content,
+    "utf-8"
   )
 }
